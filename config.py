a7b4681e2a978819e1f0c7abb633d03aeb3e17d9
WRWPhillips <42WPhillips42@gmail.com>
2022-04-19 00:12:54 -0500

    initial commit

---
+++ config.py
@@ -0,0 +1,2 @@
+USERNAME = "WRWPhillips"
+TOKEN = "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"
---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import *
+from flask_cors import CORS
+from config import *
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+if __name__ == "__main__":
+    app.run()
+
+@app.route('/projects', methods=["GET"])
+def getProjects():
+    try:
+        url = "https://api.github.com/users/WRWPhillips/repos?per_page=100"
+        url2 = "https://api.github.com/users/WRWPhillips/repos?per_page=100/page=2"
+        headers = {"Accept":"application/vnd.github.mercy-preview+json"}
+        pinned = ["nasa-photo-of-the-day-project", "ruby-pic2text", "nutritionsite", "Anytime-Fitness-Backend"]
+        repos = requests.get(url, headers=headers, auth=(USERNAME,TOKEN)).json()
+        projects = []
+        for repo in repos: 
+            if repo["stargazers_count"] >= 1: 
+                project = {
+                    "id": repo["id"],
+                    "name": repo["name"],
+                    "url": repo["html_url"],
+                    "description": repo["description"],
+                    "topics": repo["topics"],
+                    "language": repo["language"]
+                }
+                projects.append(project)
+        repos2 = requests.get(url2, headers=headers, auth=(USERNAME, TOKEN)).json()
+        for repo in repos2: 
+            if repo["stargazers_count"] >= 1: 
+                project = {
+                    "id": repo["id"],
+                    "name": repo["name"],
+                    "url": repo["html_url"],
+                    "description": repo["description"],
+                    "topics": repo["topics"],
+                    "language": repo["language"]
+                }
+                projects.append(project)
+        return {"projects": projects, "error": False}
+    except Exception as e:
+        return {"error": True, "message": str(e)}, 500
